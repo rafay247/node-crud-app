969d1225a394b4af748a30b94f2d9f5cf767f13f
RAFAY SHAIKH <59569929+rafay247@users.noreply.github.com>
2022-09-20 22:47:59 +0545

    Initial commit

--- server/database/connection.js
+++ server/database/connection.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try{
-        // mongodb connection string
-        //oncloud //mongodb+srv://rafay:rafay123@cluster0.polk0xw.mongodb.net/users?retryWrites=true&w=majority
         const con = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
